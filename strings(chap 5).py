4366a993031fcfca32871af09344a0c263ef2163
RenukaPhatak <renukaphatak05@gmail.com>
2024-06-07 17:02:59 +0530

    Add files via upload

---
+++ strings(chap 5).py
@@ -0,0 +1,27 @@
+str1 = "Hello"
+str2 = "spam"
+print(str1,str2)
+firstName  = input("Please enter your name")
+print("Hello",firstName)
+greet = "Hello Lisa"
+greet[0]
+print(greet[1] ,greet[3], greet[4])
+x = 8
+print(greet[x - 2])
+greet[-1]
+greet[-4]
+greet[0:3]
+greet[5:9]
+greet[5:]
+greet[:5]
+greet[:]
+"Honey" + "Butter"
+"Honey"+ "And" +"Butter"
+3 * "eggs"
+"eggs" * 5
+(3 * "eggs") + ("eggs" * 5)
+len("Butter")
+len("HoneyAndButter")
+for ch in "eggs!":
+    print(ch, end="")
+    
